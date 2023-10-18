c5392c3b072415abaedb39cad7da06fec4173559
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-10-18 23:28:26 +0200

    add basic functionality

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,70 @@
 const express = require("express");
 
 const router = express.Router();
+const contacts = require("../../models/contacts");
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Homework test" });
+  const result = await contacts.listContacts();
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const id = req.params.contactId;
+  const result = await contacts.getContactByID(id);
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const newContact = req.body;
+  const result = await contacts.addContact(newContact);
+
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      result,
+    },
+  });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const id = req.params.contactId;
+  const result = await contacts.removeContact(id);
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const updatedContacts = await contacts.updateContact(
+    req.params.contactId,
+    req.body
+  );
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      updatedContacts,
+    },
+  });
 });
 
 module.exports = router;
